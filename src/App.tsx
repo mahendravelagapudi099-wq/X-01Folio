dd5c5bcd1c22fd71fa1a918fe7284379999a8824
Velagapudi Mahendra <23uj1a6742@mrem.ac.in>
2026-05-11 05:46:34 +0530

    chore: trigger deployment build with new secrets

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// App.tsx
+// App.tsx - Build Triggered: 2026-05-11
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
