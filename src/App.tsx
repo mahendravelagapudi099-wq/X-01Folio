ef35a52c3caceb1652a2245bf7ee0f8669a8cc4c
Velagapudi Mahendra <23uj1a6742@mrem.ac.in>
2026-05-11 11:31:23 +0530

    fix: add HelmetProvider to prevent crash on blog post pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -17,32 +18,34 @@ const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        {/* Global Toaster / Notifications */}
-        <Toaster />
-        <Sonner />
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          {/* Global Toaster / Notifications */}
+          <Toaster />
+          <Sonner />
 
-        {/* Routing */}
-        <BrowserRouter>
-          {/* Layout wrapper */}
-          <div className="flex flex-col min-h-screen">
-            {/* Main content area */}
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/blog/:slug" element={<BlogPost />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </main>
+          {/* Routing */}
+          <BrowserRouter>
+            {/* Layout wrapper */}
+            <div className="flex flex-col min-h-screen">
+              {/* Main content area */}
+              <div className="flex-grow">
+                <Routes>
+                  <Route path="/" element={<Index />} />
+                  <Route path="/blog/:slug" element={<BlogPost />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </div>
 
-            {/* Footer and Quick Connect are rendered only once */}
-            <Footer />
-            <QuickConnect />
-          </div>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
+              {/* Footer and Quick Connect are rendered only once */}
+              <Footer />
+              <QuickConnect />
+            </div>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 };
 
